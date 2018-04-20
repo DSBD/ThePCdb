bdeea6298e8e646b3bfb8ebfc9b832ede85ccbef
Dragos <baciudavid.dragos@gmail.com>
2018-04-20 12:49:23 -0400

    Initial Folder Structure

---
+++ ThePCdb/AppCore/Warehouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePCdb.AppCore
+{
+    static class Warehouse
+    {
+        //TODO: Store All Components
+        //TODO: Program to Access Component Details by Category
+        //TODO: Methods for Each Component
+    }
+}
---
+++ ThePCdb/Components/Part.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePCdb.Components
+{
+    class Part
+    {
+        protected string Name { get; set; }
+        protected double Price { get; set; }
+        protected int Rating { get; set; }
+        protected string Type { get; set; }
+
+        public Part(string name, double price, int rating, string type)
+        {
+            Name = name;
+            Price = price;
+            Rating = rating;
+            Type = type;
+        }
+    }
+}
