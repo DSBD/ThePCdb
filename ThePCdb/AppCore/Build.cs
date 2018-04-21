c336d8b881a8fef87fdf235cf9c2672c959489a9
SteveW223 <winstanley.steven05@gmail.com>
2018-04-21 19:40:20 -0400

    Added Build Class, the fun has begun

---
+++ ThePCdb/AppCore/Build.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePCdb.AppCore
+{
+    public class Build
+    {
+        protected Components.CasePart Case { get; set; }
+        protected Components.CoolingPart Cooling { get; set; }
+        protected Components.CPUPart CPU { get; set; }
+        protected Components.GPUPart GPU { get; set; }
+        protected Components.MotherboardPart Motherboard { get; set; }
+        protected Components.PSUPart PSU { get; set; }
+        protected Components.RAMPart RAMKit { get; set; }
+        protected Components.StoragePart Storage { get; set; }
+        public Build(Components.CasePart _case, Components.CoolingPart cooling, Components.CPUPart cpu, Components.GPUPart gpu, Components.MotherboardPart motherboard, Components.PSUPart psu, Components.RAMPart ram, Components.StoragePart storage)
+        {
+            Case = _case;
+            Cooling = cooling;
+            CPU = cpu;
+            GPU = gpu;
+            Motherboard = motherboard;
+            PSU = psu;
+            RAMKit = ram;
+            Storage = storage;
+        }
+    }
+}
