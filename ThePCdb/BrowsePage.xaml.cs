a074d04b00dcd5e06c572b2c08053711221dbe67
SteveW223 <winstanley.steven05@gmail.com>
2018-04-23 18:01:41 -0400

    gg

--- ThePCdb/BrowsePage.xaml.cs
+++ ThePCdb/BrowsePage.xaml.cs
@@ -122,7 +122,7 @@ namespace ThePCdb
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(PartInfo));
+            
         }
     }
 }
